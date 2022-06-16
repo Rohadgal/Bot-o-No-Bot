cb6a6d2302fd4788eb4e76964e776492a91d1baf
lfeq <panchocosa@gmail.com>
2022-06-16 09:40:48 -0500

    Avances pistas

--- Bot o No Bot/Assets/Scripts/PistasManager.cs
+++ Bot o No Bot/Assets/Scripts/PistasManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PistasManager : MonoBehaviour
 {
+    public TMP_Text textSiguiendo, textSeguidores;
+    public Image imagenComentario1, imagenComentario2, imagenComentario3;
+
     // Start is called before the first frame update
     void Start()
     {
