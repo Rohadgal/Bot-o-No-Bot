466cc5fe6ff239b82ad59c6ea792eba1083ec7bc
Gal <gal.frias@gmail.com>
2022-06-16 12:51:23 -0500

    Script selección
    Script selección de Bot o no Bot

--- Bot o No Bot/Assets/Scripts/GameManager.cs
+++ Bot o No Bot/Assets/Scripts/GameManager.cs
@@ -5,21 +5,52 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<Usuario> comentarios = new List<Usuario>();
-
+    Usuario currentUsuario;
 
     // Start is called before the first frame update
     void Start()
     {
-        Usuario nuevoUsuario = new Usuario();
-        comentarios.Add(nuevoUsuario);
-        comentarios.RemoveAt(0);
+        //Usuario nuevoUsuario = new Usuario();
+        //comentarios.Add(nuevoUsuario);
+        //RandomEleccion();
+        //ChecadorRespuesta();
+        //comentarios.RemoveAt(0);
 
-        Random.Range(0, comentarios.Count);
+       Random.Range(0, comentarios.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public Usuario RandomEleccion()
+    {
+        currentUsuario = comentarios[Random.Range(0, comentarios.Count)];
+        Debug.Log("Elección random");
+
+        return currentUsuario;
+        
+    }
+
+    public void ChecadorRespuesta(int eleccion)
+    {
+
+        
+        if(currentUsuario.siEsBot && eleccion == 1 )
+        {
+            Debug.Log("Es bot");
+            Debug.Log("Punto a favor para jugador");
+        }
+        else if(!currentUsuario.siEsBot && eleccion == 0)
+        {
+            Debug.Log("No es bot");
+            Debug.Log("Punto para el jugador");
+        }
+        else
+        {
+            Debug.Log("Punto para el bot");
+        }
     }
 }
